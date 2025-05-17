da163da6ad90f5a4287905491dac566ce63609d3
contact <contact@loayidwan.com>
2025-05-17 20:52:35 +0000

    more comments removal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/main_navigator.dart'; // Import the main navigator
+import 'pages/main_navigator.dart';
 
 
 void main() {
@@ -8,16 +8,12 @@ void main() {
   );
 }
 
-// The root widget of the application.
-// It's a StatelessWidget because its own configuration doesn't change over time.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
         return const MaterialApp(
-          // Title of the app (shown in task manager, etc.)
           title: 'Flutter Bookstore',
           debugShowCheckedModeBanner: false,
           home: MainNavigator(),
